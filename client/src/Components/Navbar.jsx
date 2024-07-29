776964b1aa9c0db9a63084b02105a4f9a3f5865c
Vengateshwaran1 <138496009+Vengateshwaran1@users.noreply.github.com>
2024-07-29 10:05:51 +0530

    Update Navbar.jsx

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -25,29 +25,29 @@ const Navbar = () => {
       <h1 className='w-full text-3xl font-bold text-primary'>Fasten Your Belt</h1>
       <ul className='hidden md:flex'>
         <li
-          className={`p-4 hover:border hover:rounded-lg cursor-pointer ${
-            location.pathname === '/' ? 'border rounded-lg' : ''
+          className={`p-4 hover:text-primary hover:underline cursor-pointer ${
+            location.pathname === '/' ? 'text-primary underline' : ''
           }`}
           onClick={handleHomeClick}
         >
           Home
         </li>
         <li
-          className={`p-4 hover:border hover:rounded-lg cursor-pointer ${
-            location.pathname === '/booking' ? 'border rounded-lg' : ''
+          className={`p-4 hover:text-primary hover:underline cursor-pointer ${
+            location.pathname === '/booking' ? 'text-primary underline' : ''
           }`}
           onClick={handleBookingClick}
         >
           Booking
         </li>
-        <li className='p-4 hover:border hover:rounded-lg cursor-pointer'>About</li>
-        <li className='p-4 rounded-lg hover:border cursor-pointer bg-primary'>Login/Signup</li>
+        <li className='p-4 hover:text-primary hover:underline cursor-pointer'>About</li>
+        <li className='p-4 hover:underline cursor-pointer bg-primary rounded-xl'>Login/Signup</li>
       </ul>
 
       <div onClick={handleNav} className='block md:hidden'>
         {nav ? <img src={closeImg} alt="" className='w-8' /> : <img src={menuImg} className='w-8' />}
       </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-primary backdrop-blur-[2px] bg-opacity-20 ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+      <ul className={nav ? ' fixed z-30  left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-primary backdrop-blur-[2px] bg-opacity-20 ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
         <li className='p-4 border-b-2 border-black' onClick={handleHomeClick}>Home</li>
         <li className='p-4 border-b-2 border-black' onClick={handleBookingClick}>Booking</li>
         <li className='p-4 border-b-2 border-black'>About</li>

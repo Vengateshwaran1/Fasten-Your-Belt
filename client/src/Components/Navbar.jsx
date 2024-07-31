04e9b8023299d0f8a6676eeebb7c95efe122100a
vengateshwaran1 <kvengateshwaran1@gmail.com>
2024-07-31 11:09:40 +0530

    commit

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -20,9 +20,13 @@ const Navbar = () => {
     navigate('/');
   };
 
+  const handlePublishClick = () => {
+    navigate('/publish');
+  };
+
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className='w-full text-3xl font-bold text-primary'>Fasten Your Belt</h1>
+      <h1 className='w-full text-4xl font-bold text-primary'>Fasten Your Belt</h1>
       <ul className='hidden md:flex'>
         <li
           className={`p-4 hover:text-primary hover:underline cursor-pointer ${
@@ -40,6 +44,14 @@ const Navbar = () => {
         >
           Booking
         </li>
+        <li
+          className={`p-4 hover:text-primary hover:underline cursor-pointer text-center ${
+            location.pathname === '/publish' ? 'text-primary underline' : ''
+          }`}
+          onClick={handlePublishClick}
+        >
+          Publish ride
+        </li>
         <li className='p-4 hover:text-primary hover:underline cursor-pointer'>About</li>
         <li className='p-4 hover:underline cursor-pointer bg-primary rounded-xl'>Login/Signup</li>
       </ul>
@@ -47,9 +59,10 @@ const Navbar = () => {
       <div onClick={handleNav} className='block md:hidden'>
         {nav ? <img src={closeImg} alt="" className='w-8' /> : <img src={menuImg} className='w-8' />}
       </div>
-      <ul className={nav ? ' fixed z-30  left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-primary backdrop-blur-[2px] bg-opacity-20 ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+      <ul className={nav ? 'fixed z-30  left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-primary backdrop-blur-[3px] bg-opacity-25 ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
         <li className='p-4 border-b-2 border-black' onClick={handleHomeClick}>Home</li>
         <li className='p-4 border-b-2 border-black' onClick={handleBookingClick}>Booking</li>
+        <li className='p-4 border-b-2 border-black' onClick={handlePublishClick}>Publish ride</li>
         <li className='p-4 border-b-2 border-black'>About</li>
         <li className='p-4 border-b-2 border-black'>Login/Signup</li>
       </ul>
--- client/src/Publish.jsx
+++ client/src/Publish.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import Layout from './Components/Layout'
 
 const Publish = () => {
+    const steps = [
+      {
+        step: 1,
+        title: "Create your account",
+        description: "Add your profile picture, a few words about you and your phone number to increase trust between members."
+      },
+      {
+        step: 2,
+        title: "Publish a ride",
+        description: "Indicate departure and arrival points, the date of the ride and check our recommended price to increase your chances of getting your first passengers and ratings."
+      },
+      {
+        step: 3,
+        title: "Enjoy the ride",
+        description: "That's how easy it is to start saving on travel costs!"
+      },
+    ]
   return (
-    <div>Publish</div>
+    <Layout>
+      <section>
+      <h1 className="text-2xl text-left p-4 font-bold"> <span className="text-primary"> Publish </span> <span className='text-white'>a</span> <span className="text-primary"> Ride </span> <span className='text-white'> in Just </span><span className="text-primary">Minutes</span></h1>
+      <div className="container pt-6 max-w-screen-xl pb-16 mx-auto md:justify-start flex flex-col md:flex-row items-start p-4">
+        <div className="md:w-1/2 w-full justify-start mb-10 lg:mb-0">
+          <div className="flex flex-col flex-wrap gap-5 px-0 md:px-10 sm:py-0 text-left">
+            {steps.map(step => 
+              <div key={step.step} className="flex flex-col md:items-start">
+                <div className="w-8 h-8 inline-flex items-center justify-left text-primary mb-2">{step.step}</div>
+                <div className="flex-grow">
+                  <h2 className="text-lg title-font font-medium mb-3 text-gray-400">{step.title}</h2>
+                  <p className="leading-relaxed text-muted-foreground text-base text-white">{step.description}</p>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="md:w-1/2 w-full">
+            
+        
+        </div>
+      </div>
+    </section>
+    </Layout>
   )
 }
 
